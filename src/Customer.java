ae5b8b0e726c16a255f08fae969f742fe10fcdbc
kha1dx <162775528+kha1dx@users.noreply.github.com>
2025-07-04 17:39:44 +0300

    updating customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,6 +1,7 @@
 public class Customer {
     private String name;
     private double balance;
+
     public Customer(String name, double balance) {
         this.name = name;
         this.balance = balance;
@@ -18,11 +19,13 @@ public class Customer {
             System.out.println("Amount to add must be positive.");
         }
     }
-    public void deductBalance(double amount) {
+    public boolean deductBalance(double amount) {
         if (amount > 0 && amount <= balance) {
             this.balance -= amount;
+            return true;
         } else {
             System.out.println("Insufficient balance or invalid amount.");
+            return false;
         }
     }
 }
